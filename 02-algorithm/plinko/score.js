542976a49721e4777aa963bbe8c25deaa56f9c36
Noah Kim <dongwoo_333@hanmail.net>
2022-12-11 09:25:40 +1100

    16. Testing the Algorithm

--- 02-algorithm/plinko/score.js
+++ 02-algorithm/plinko/score.js
@@ -1,12 +1,31 @@
 const outputs = [];
+const predictionPoint = 300;
+const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
-
-  console.log(outputs);
 }
 
 function runAnalysis() {
+  console.log('the length of data: ', outputs.length);
+
   // Write code here to analyze stuff
+  const bucket = _.chain(outputs)
+    .map(row => [distance(row[0]), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+
+  console.log('Your point will probably fall into', bucket);
+}
+
+function distance(point) {
+  return Math.abs(point - predictionPoint);
 }
