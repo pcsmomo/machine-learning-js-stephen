a279414f18c05d89d0cf71145142705fd1751538
Noah Kim <dongwoo_333@hanmail.net>
2022-12-12 19:44:43 +1100

    31. Normalization with MinMax

--- 02-algorithm/plinko/score.js
+++ 02-algorithm/plinko/score.js
@@ -89,3 +89,26 @@ function splitDataset(data, testCount) {
 
   return [testSet, trainingSet];
 }
+
+function minMax(data, featureCount) {
+  // [
+  //   [10, 0.5, 16, 1],
+  //   [200, 0.5, 16, 4],
+  //   [350, 0.5, 16, 4],
+  //   [600, 0.5, 16, 5]
+  // ];
+  const clonedData = _.cloneDeep(data);
+
+  for (let i = 0; i < featureCount; i++) {
+    const column = clonedData.map(row => row[i]);
+
+    const min = _.min(column);
+    const max = _.max(column);
+
+    for (let j = 0; j < clonedData.length; j++) {
+      clonedData[j][i] = (clonedData[j][i] - min) / (max - min);
+    }
+  }
+
+  return clonedData;
+}
