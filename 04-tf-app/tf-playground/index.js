1bdc6b02def88fe9c7e08b68f9510747750c2b8e
Noah Kim <dongwoo_333@hanmail.net>
2022-12-14 08:22:50 +1100

    54. Averaging Top Values

--- 04-tf-app/tf-playground/index.js
+++ 04-tf-app/tf-playground/index.js
@@ -15,13 +15,35 @@ features.print();
 labels.print();
 predictionPoint.print();
 
+const k = 2;
+
+console.log('== all in one ==');
+const average =
+  features
+    .sub(predictionPoint)
+    .pow(2)
+    .sum(1)
+    .pow(0.5)
+    .expandDims(1)
+    .concat(labels, 1)
+    .unstack()
+    .sort((a, b) => (a.arraySync()[0] > b.arraySync()[0] ? 1 : -1))
+    .slice(0, k)
+    .reduce((acc, pair) => {
+      return acc + pair.arraySync()[1]; // the house value
+    }, 0) / k;
+console.log('average:', average);
+
+console.log('\n == details ==');
 // 51. KNN with Tensorflow
 // distance = ((lon - lon) ** 2 + (lat - lat) ** 2) \*\* 0.5
 const distances = features.sub(predictionPoint).pow(2).sum(1).pow(0.5);
+console.log('diatance >');
 distances.print();
 
 // 52. Maintaining Order Relationships
 const pairedData = distances.expandDims(1).concat(labels, 1);
+console.log('pairedData >');
 pairedData.print();
 
 // 53. Sorting Tensors
@@ -31,10 +53,27 @@ pairedData.print();
 // pairedData.unstack()[2].print();
 // pairedData.unstack()[3].print();
 
+// now the list is a normal javascript array, Tensor[]
+// but the elements are Tensor object
 const sorted = pairedData
   .unstack()
   .sort((a, b) => (a.arraySync()[0] > b.arraySync()[0] ? 1 : -1)); // ascending
+console.log('sorted >');
 sorted[0].print();
 sorted[1].print();
 sorted[2].print();
 sorted[3].print();
+
+// 54. Averaging Top Values
+console.log('k:', k);
+const sliced = sorted.slice(0, k);
+// sliced[0].print();
+// sliced[1].print();
+
+const acc = sliced.reduce((acc, pair) => {
+  return acc + pair.arraySync()[1]; // the house value
+}, 0);
+console.log('acc:', acc);
+
+const avg = acc / k;
+console.log('avg:', avg);
