70447c772f044320d055ac0723ebd0c5a87cfd86
Noah Kim <dongwoo_333@hanmail.net>
2022-12-15 07:52:57 +1100

    57. Running an Analysis

--- 04-tf-app/knn-tf-house-price/index.js
+++ 04-tf-app/knn-tf-house-price/index.js
@@ -2,6 +2,24 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 
+function knn(features, labels, predictionPoint, k) {
+  return (
+    features
+      .sub(predictionPoint)
+      .pow(2)
+      .sum(1)
+      .pow(0.5)
+      .expandDims(1)
+      .concat(labels, 1)
+      .unstack()
+      .sort((a, b) => (a.arraySync()[0] > b.arraySync()[0] ? 1 : -1))
+      .slice(0, k)
+      .reduce((acc, pair) => {
+        return acc + pair.arraySync()[1]; // the house value
+      }, 0) / k
+  );
+}
+
 let { features, labels, testFeatures, testLabels } = loadCSV(
   'kc_house_data.csv',
   {
@@ -12,5 +30,14 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
   }
 );
 
-console.log(testFeatures);
-console.log(testLabels);
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+// testFeatures = tf.tensor(testFeatures);
+// testLabels = tf.tensor(testLabels);
+
+// let's start it with only one predictionPoint
+predictionPoint = tf.tensor(testFeatures[0]);
+
+const result = knn(features, labels, predictionPoint, 10);
+console.log('Guess', result, testLabels[0][0]);
+// Guess 1421200 1085000
