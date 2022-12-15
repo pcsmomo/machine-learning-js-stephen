786b1ad1fc2212a2a1984e4b02b522164cffdb29
Noah Kim <dongwoo_333@hanmail.net>
2022-12-15 18:10:15 +1100

    188. Splitting Test and Training

--- 14-loadcsv/loadcsv/load-csv.js
+++ 14-loadcsv/loadcsv/load-csv.js
@@ -13,7 +13,13 @@ function extractColumns(data, columnNames) {
 
 function loadCSV(
   filename,
-  { converters = {}, dataColumns = [], labelColumns = [], shuffle = true }
+  {
+    converters = {},
+    dataColumns = [],
+    labelColumns = [],
+    shuffle = true,
+    splitTest = false
+  }
 ) {
   let data = fs.readFileSync(filename, { encoding: 'utf-8' });
   data = data.split('\n').map(row => row.split(','));
@@ -52,14 +58,35 @@ function loadCSV(
 
   console.log(labels);
   console.log(data);
+
+  if (splitTest) {
+    const trainSize = _.isNumber(splitTest)
+      ? splitTest
+      : Math.floor(data.length / 2); // half
+
+    return {
+      features: data.slice(0, trainSize),
+      labels: labels.slice(0, trainSize),
+      testFeatures: data.slice(trainSize),
+      testLabels: labels.slice(trainSize)
+    };
+  } else {
+    return { features: data, labels };
+  }
 }
 
-loadCSV('data.csv', {
+const { features, labels, testFeatures, testLabels } = loadCSV('data.csv', {
   dataColumns: ['height', 'value'],
   labelColumns: ['passed'],
   shuffle: true,
+  splitTest: false, // number | boolean
   converters: {
     passed: val => val === 'TRUE' // ? true : false
     // passed: val => (val === 'TRUE' ? 1 : 0)
   }
 });
+
+console.log('Features', features);
+console.log('Labels', labels);
+console.log('testFeatures', testFeatures);
+console.log('testLabels', testLabels);
