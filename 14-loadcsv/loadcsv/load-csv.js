c155255d451f4ffa0df435f2a8c354688e675a5d
Noah Kim <dongwoo_333@hanmail.net>
2022-12-15 17:15:17 +1100

    182. Splitting into Columns

--- 14-loadcsv/loadcsv/load-csv.js
+++ 14-loadcsv/loadcsv/load-csv.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 function loadCSV(filename, options) {
   const data = fs.readFileSync(filename, { encoding: 'utf-8' });
-  console.log(data);
+  console.log(data.split('\n').map(row => row.split(',')));
 }
 
 loadCSV('data.csv');
