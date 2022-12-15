b63aacee3c91218b4c290e37945190eaf30d7aea
Noah Kim <dongwoo_333@hanmail.net>
2022-12-15 17:20:29 +1100

    184. Parsing Number Values

--- 14-loadcsv/loadcsv/load-csv.js
+++ 14-loadcsv/loadcsv/load-csv.js
@@ -5,6 +5,19 @@ function loadCSV(filename, options) {
   let data = fs.readFileSync(filename, { encoding: 'utf-8' });
   data = data.split('\n').map(row => row.split(','));
   data = data.map(row => _.dropRightWhile(row, val => val === '')); // drop trailing commas
+  const headers = _.first(data);
+
+  data = data.map((row, index) => {
+    if (index === 0) {
+      return row;
+    }
+
+    return row.map((element, index) => {
+      const result = parseFloat(element);
+      return _.isNaN(result) ? element : result;
+    });
+  });
+
   console.log(data);
 }
 
