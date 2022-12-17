f321227349519e586b75f360ed42f2d9d1839c15
Noah Kim <dongwoo_333@hanmail.net>
2022-12-18 07:28:06 +1100

    77. Data Loading

---
+++ 06-gradient-descent-tf/regressions/index.js
@@ -0,0 +1,9 @@
+const tf = require('@tensorflow/tfjs-node');
+const loadCSV = require('./load-csv');
+
+let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
+  shuffle: true,
+  splitTest: 50,
+  dataColumns: ['horsepower'],
+  labelColumns: ['mpg']
+});
