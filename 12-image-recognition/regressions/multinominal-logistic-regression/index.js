e6fdecf51668e51d8d0e0f08b7ebba7f456b8d50
Noah Kim <dongwoo_333@hanmail.net>
2022-12-24 07:59:03 +1100

    153. Implementing an Accuracy Gauge

--- 12-image-recognition/regressions/multinominal-logistic-regression/index.js
+++ 12-image-recognition/regressions/multinominal-logistic-regression/index.js
@@ -2,8 +2,9 @@ const tf = require('@tensorflow/tfjs-node');
 const plot = require('node-remote-plot'); // generate an image
 const _ = require('lodash');
 const mnist = require('mnist-data');
+const LogisticRegression = require('./logistic-regression');
 
-const mnistData = mnist.training(0, 10);
+const mnistData = mnist.training(0, 1000);
 
 // console.log(mnistData.images.values);
 
@@ -16,5 +17,24 @@ const encodedLabels = mnistData.labels.values.map(label => {
   row[label] = 1;
   return row;
 });
+// console.log(encodedLabels);
 
-console.log(encodedLabels);
+const regression = new LogisticRegression(features, encodedLabels, {
+  learningRate: 1,
+  iterations: 5,
+  batchSize: 100
+});
+
+regression.train();
+
+const testMnistData = mnist.testing(0, 100);
+// console.log(testMnistData);
+const testFeatures = testMnistData.images.values.map(image => _.flatMap(image));
+const testEncodedLabels = testMnistData.labels.values.map(label => {
+  const row = new Array(10).fill(0);
+  row[label] = 1;
+  return row;
+});
+
+const accuracy = regression.test(testFeatures, testEncodedLabels);
+console.log('Accuracy is', accuracy);
