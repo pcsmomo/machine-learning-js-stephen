2217993865c0cbd7ca0b7714f90e6ecf49e40d78
Noah Kim <dongwoo_333@hanmail.net>
2022-12-23 19:12:25 +1100

    141. Classifying Continuous Values

--- 11-multi-value-classification/regressions/multinominal-logistic-regression/index.js
+++ 11-multi-value-classification/regressions/multinominal-logistic-regression/index.js
@@ -7,23 +7,35 @@ const { features, labels, testFeatures, testLabels } = loadCSV(
   '../data/cars.csv',
   {
     dataColumns: ['horsepower', 'displacement', 'weight'],
-    labelColumns: ['passedemissions'],
+    labelColumns: ['mpg'],
     shuffle: true,
     splitTest: 50,
     converters: {
-      passedemissions: val => (val === 'TRUE' ? 1 : 0)
+      mpg: value => {
+        const mpg = parseFloat(value);
+
+        if (mpg < 15) {
+          return [1, 0, 0];
+        } else if (mpg < 30) {
+          return [0, 1, 0];
+        } else {
+          return [0, 1, 1];
+        }
+      }
     }
   }
 );
 
-const regression = new LogisticRegression(features, labels, {
-  learningRate: 0.5,
-  iterations: 100,
-  batchSize: 5, // 50
-  decisionBoundary: 0.5
-});
+console.log(labels);
 
-regression.train();
+// const regression = new LogisticRegression(features, labels, {
+//   learningRate: 0.5,
+//   iterations: 100,
+//   batchSize: 5, // 50
+//   decisionBoundary: 0.5
+// });
+
+// regression.train();
 
 // dataColumns: ['horsepower', 'displacement', 'weight'],
 // regression
@@ -33,9 +45,9 @@ regression.train();
 //   ])
 //   .print();
 
-console.log(regression.test(testFeatures, testLabels)); // percentage: 88% (not bad!)
+// console.log(regression.test(testFeatures, testLabels)); // percentage: 88% (not bad!)
 
-console.log(regression.costHistory);
-plot({
-  x: regression.costHistory.reverse()
-});
+// console.log(regression.costHistory);
+// plot({
+//   x: regression.costHistory.reverse()
+// });
